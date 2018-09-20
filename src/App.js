c93c55497edd0dc9719ba559f5a24dbcd36f7744
CherieLee-Mason <clsoar@gmail.com>
2018-09-20 10:26:00 -0500

    Map boilerplate component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Map from './components/Map.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,12 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">My Map App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Map/>
+
       </div>
     );
   }
