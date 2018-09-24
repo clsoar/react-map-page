bb8ea9cf3cad9945f5ffcee344af1356d002f4df
CherieLee-Mason <clsoar@gmail.com>
2018-09-24 10:57:42 -0500

    Map is gray issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Map from './components/Map.js';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- src/components/Map.js
+++ src/components/Map.js
@@ -25,7 +25,7 @@ class Map extends Component {
         My Map Here
         <MyMapComponent
          isMarkerShown
-         googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnDRTGBF8TMfvIBEZeAX3wDPaDZ24RxUE&v=3.exp&libraries=geometry,drawing,places"
          loadingElement={<div style={{ height: `100%` }} />}
          containerElement={<div style={{ height: `400px` }} />}
          mapElement={<div style={{ height: `100%` }} />}
