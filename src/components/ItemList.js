fe93e98c8d692ca69589f21590d291804541a064
CherieLee-Mason <clsoar@gmail.com>
2018-10-04 22:56:30 -0500

    ItemList component started

---
+++ src/components/ItemList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import './Map.css';
+import {Markers} from './markers.js';
+
+class ItemList extends Component {
+
+  render(){
+    return(
+      
+    )
+  }
+}
+export default ItemList;
