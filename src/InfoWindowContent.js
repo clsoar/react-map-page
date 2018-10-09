e0e7ce7da6ed38e0b35e17a573679ca60d7eb1a3
CherieLee-Mason <clsoar@gmail.com>
2018-10-09 14:42:51 -0500

    InfoWindowContent added and refresh bug fixed

--- src/InfoWindowContent.js
+++ src/InfoWindowContent.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
 import './Map.css';
 
+const OnsiteFood = (props) =>
+
+  <ul className="restaurant-info">
+    <li className="restaurant-name">{props.marker.restaurant.name}</li>
+    <li className="restaurant-website"><a href={props.marker.restaurant.website}>Restaurant Website</a></li>
+
+
+  </ul>
+
+
 class InfoWindowContent extends Component {
+
+  amentiesCheck = (amenity) => {
+    let answer = "yes";
+    amenity ? answer="yes" : answer="no";
+    return answer
+    }
+
+  restaurantInfoAdder = (food) => {
+    let foodAnswer = "checking";
+    const noFood = null;
+
+    food ? foodAnswer=<OnsiteFood /> : foodAnswer=noFood;
+    return foodAnswer;
+  }
+
 
   render(){
     return (
       <div className="window-content">
         <h3 className="window-heading">{this.props.marker.name}</h3>
-        <img src="#" alt="Image text" className="window-image"/>
+        <img src="#" alt="Alt text" className="window-image"/>
         <div className="window-address">{this.props.marker.address}</div>
+        <div className="window-website"><a target="_blank" href={this.props.marker.website}>Accomadation Website</a></div>
+        <div className="window-phone">{this.props.marker.phone}</div>
+        <div className="type-building">Accomadation Type: {this.props.marker.typeOfBuilding}</div>
+        <div className="amenities">
+          <ul className="amenities-list">
+            <li className="BeachAccess">Beach Front: {this.amentiesCheck(this.props.marker.beachAccess)}</li>
+            <li className="Pool">Pool: {this.amentiesCheck(this.props.marker.pool)}</li>
+            <li className="fitness-center">Fitness Center: {this.amentiesCheck(this.props.marker.fitnessCenter)}</li>
+            <li className="tennis-court">Tennis Courts: {this.amentiesCheck(this.props.marker.tennisCourt)}</li>
+            <li className="parking">Parking: {this.amentiesCheck(this.props.marker.parking)}</li>
+            <li className="restaurant">Restaurant on Site: {this.amentiesCheck(this.props.marker.restaurant.value)}</li>
 
+          </ul>
+          {this.props.marker.restaurant.value && <OnsiteFood {...this.props}/>}
+        </div>
+        <div className="description">{this.props.marker.description}</div>
       </div>
 
     )
--- src/Map.js
+++ src/Map.js
@@ -5,6 +5,39 @@ import './Map.css';
 import {FaAnchor} from 'react-icons/fa';
 import InfoWindowContent from './InfoWindowContent.js';
 
+const MyMapComponent = withScriptjs(withGoogleMap((props, state) =>
+  <GoogleMap
+    defaultZoom = {15}
+    defaultCenter = {{ lat: 30.381025, lng: -86.866819 }}
+    >
+    {(props.error) ?
+        <InfoWindow
+          position= {state.center}>
+          <p className="error-message">{props.error.message}</p>
+        </InfoWindow>
+        :
+        props.isMarkerShown &&
+      /* added .map to add latlng data from extendable object*/
+      props.markers.map((marker) => (
+        <Marker
+          key={marker.id}
+          position= {{ lat: marker.latlng.lat, lng: marker.latlng.lng }}
+          onClick={() => {props.onToggleOpen(marker)}}
+          animation= {marker.isOpen && google.maps.Animation.DROP}
+        >
+          {marker.isOpen &&
+          <InfoWindow
+          >
+            <InfoWindowContent
+              marker = { marker }
+            />
+          </InfoWindow>}
+        </Marker>
+      ))
+    }
+
+  </GoogleMap>
+))
 
 class Map extends Component {
 
@@ -20,43 +53,12 @@ class Map extends Component {
     taken from react-google-maps installation and usage documentation
     https://tomchentw.github.io/react-google-maps/#installation
     */
-    const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-      <GoogleMap
-        defaultZoom = {this.state.zoom}
-        defaultCenter = {this.state.center}
-        >
-        {(this.props.error) ?
-            <InfoWindow
-              position= {this.state.center}>
-              <p className="error-message">{this.props.error.message}</p>
-            </InfoWindow>
-            :
-            props.isMarkerShown &&
-          /* added .map to add latlng data from extendable object*/
-          this.props.markers.map((marker) => (
-            <Marker
-              key={marker.id}
-              position= {{ lat: marker.latlng.lat, lng: marker.latlng.lng }}
-              onClick={() => {this.props.onToggleOpen(marker)}}
-              animation= {marker.isOpen && google.maps.Animation.DROP}
-            >
-              {marker.isOpen &&
-              <InfoWindow
-              >
-                <InfoWindowContent
-                  marker = { marker }
-                />
-              </InfoWindow>}
-            </Marker>
-          ))
-        }
 
-      </GoogleMap>
-    ))
 
     return (
       <div className="Map">
         <MyMapComponent
+          {...this.props}
           center = {this.state.center}
           zoom = {this.state.zoom}
           isMarkerShown
